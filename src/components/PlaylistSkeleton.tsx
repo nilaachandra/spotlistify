485cedc7e9333e2e1da31fc2e9caa1bd63a0c2ec
Nila Chandra Laishram <nilalaishram@gmail.com>
2024-08-26 22:10:02 +0530

    skeleton fix

--- src/components/PlaylistSkeleton.tsx
+++ src/components/PlaylistSkeleton.tsx
@@ -5,11 +5,11 @@ const PlaylistSkeleton = () => {
   return (
     <div className="w-full flex p-3 gap-2 cursor-pointer bg-zinc-900 rounded-lg">
       <Skeleton className="bg-zinc-700 w-[84px] h-[84px] rounded-lg" />
-      <div className="w-[512px] flex flex-col gap-2 justify-center">
-        <Skeleton className="bg-zinc-700 w-32 h-3" />
-        <Skeleton className="bg-zinc-700 w-96 h-3" />
-        <Skeleton className="bg-zinc-700 w-80 h-3" />
-        <Skeleton className="bg-zinc-700 w-60 h-3" />
+      <div className="flex flex-col gap-2 justify-center">
+        <Skeleton className="bg-zinc-700 w-24 h-3" />
+        <Skeleton className="bg-zinc-700 w-48 h-3" />
+        <Skeleton className="bg-zinc-700 w-64 h-3" />
+        <Skeleton className="bg-zinc-700 w-48 h-3" />
       </div>
     </div>
   );
