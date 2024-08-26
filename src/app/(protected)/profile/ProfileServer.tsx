65b1305c3cdb7e32cf8b034c5f1dbb19d8425107
Nila Chandra Laishram <nilalaishram@gmail.com>
2024-08-26 22:17:58 +0530

    mninor changes

--- src/app/(protected)/profile/ProfileServer.tsx
+++ src/app/(protected)/profile/ProfileServer.tsx
@@ -2,6 +2,7 @@
 import { auth } from "@/lib/auth";
 import ProfileClient from "./ProfileClient";
 import { db } from "@/lib/db";
+import Link from "next/link";
 
 // Server component to fetch data and pass it to the client component
 export default async function ProfileServer() {
@@ -10,19 +11,30 @@ export default async function ProfileServer() {
   if (!session?.user) {
     // Handle the case where the user is not authenticated
     // You can redirect or return a message
-    return <div>You are not logged in.</div>;
+    return (
+      <div className="flex flex-col min-h-[60vh] h-full items-center justify-center text-lg font-semibold">
+        <p>Oops</p>
+        <p>You are not logged in!</p>
+        <p>
+          Please{" "}
+          <Link href={"/auth/login"} className="text-green underline">
+            Log In!
+          </Link>
+        </p>
+      </div>
+    );
   }
 
   // Fetch user profile data
   const profileData = await db.user.findUnique({
     where: {
-      id: session.user.id
+      id: session.user.id,
     },
     include: {
-        playlists: true,
-        comments: true,
-        likes: true
-      }
+      playlists: true,
+      comments: true,
+      likes: true,
+    },
   });
 
   // Pass the fetched profile data to the client component
